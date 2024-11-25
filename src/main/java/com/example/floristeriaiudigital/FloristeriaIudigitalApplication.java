de4eab60e9742a9ff106dc13d96918202783b1a8
juanzapata43 <juan.zapata43@est.iudigital.edu.co>
2024-11-25 13:14:29 -0500

    Cambio en estructura

---
+++ src/main/java/com/example/floristeriaiudigital/FloristeriaIudigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.floristeriaiudigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristeriaIudigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloristeriaIudigitalApplication.class, args);
+    }
+}
+
